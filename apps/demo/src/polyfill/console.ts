ac9afefac43db878827b0f4cdceab0d8694096dd
ArcherCube <archercube@163.com>
2025-12-26 15:22:45 +0800

    feat(@heathen-apps/demo): console覆盖增加info

--- apps/demo/src/polyfill/console.ts
+++ apps/demo/src/polyfill/console.ts
@@ -2,6 +2,7 @@ import { Logger } from '@heathen/utils';
 
 // 覆盖 原生console
 console.log = Logger.log;
+console.info = Logger.log;
 console.error = Logger.error;
 console.warn = Logger.warn;
 console.group = Logger.group;
